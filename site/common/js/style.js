16344376705e98b872e29e84e9cadbeb1d553ceb
nakano keiji <filipe.keiji@gmail.com>
2016-02-16 16:32:45 +0900

    ナビゲーションとトーナメント表を更新

--- site/common/js/style.js
+++ site/common/js/style.js
@@ -10,9 +10,52 @@ $(function(){
       // 移動先を取得
       var target = $(href == "#" || href == "" ? 'html' : href);
       // 移動先を数値で取得
-      var position = target.offset().top - 70;
+      var position = target.offset().top - 128;
       // スムーススクロール
       $('body,html').animate({scrollTop:position}, speed, 'swing');
       return false;
    });
+});
+
+// FIXED NAV
+
+$(function() {
+   var nav = $('.scrollNav');
+   var header = $('.mod-header');
+   //表示位置
+   var navTop = nav.offset().top-56;
+   var navHeight = nav.height();
+   var showFlag = false;
+   nav.css('top', -navHeight+'px');
+   //ナビゲーションの位置まできたら表示
+   $(window).scroll(function () {
+       var winTop = $(this).scrollTop();
+       if (winTop >= navTop) {
+           if (showFlag == false) {
+               showFlag = true;
+               header
+                  .addClass('mod-header--fixed');
+               nav
+                  .addClass('scrollNav--fixed')
+                  .stop().animate({'top' : '40px'}, 200);
+           }
+       } else if (winTop <= navTop) {
+           if (showFlag) {
+               showFlag = false;
+               header
+                  .removeClass('mod-header--fixed');
+               nav.stop().animate({'top' : -navHeight+'px'}, 200, function(){
+                   nav.removeClass('scrollNav--fixed');
+               });
+           }
+       }
+   });
+});
+
+// TOGGLE
+
+$(function(){
+    $(".tournament-table__button__content").click(function(){
+        $(".tournament-tableContent").toggle();
+    });
 });
